8f9d9534fbd7d1e04178b338893c26564e1a136d
Angelo Ricardo <angeloengcomp@gmail.com>
2022-07-03 16:35:33 -0300

    tests: passed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,29 +8,27 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
-
 interface Image {
-  title: string,
-  description: string,
+  title: string;
+  description: string;
   url: string;
-  ts: number,
-  id: string,
+  ts: number;
+  id: string;
 }
 
 interface GetImagesResponse {
-  after: string,
-  data: Image[]
+  after: string;
+  data: Image[];
 }
 
 export default function Home(): JSX.Element {
-
   async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
     const { data } = await api('/api/images', {
       params: {
-        after: pageParam
-      }
+        after: pageParam,
+      },
     });
-    return data
+    return data;
   }
 
   const {
@@ -40,33 +38,37 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images', fetchImages, {
-    getNextPageParam: lastPage => lastPage?.after || null
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
   });
 
   const formattedData = useMemo(() => {
     const formatted = data?.pages.flatMap(imageData => {
-      return imageData.data.flat()
-    })
-    return formatted
+      return imageData.data.flat();
+    });
+    return formatted;
   }, [data]);
 
   if (isLoading && !isError) {
-    return <Loading />
-  }
-  if (isLoading && isError) {
-    return <Error />
+    return <Loading />;
   }
 
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
